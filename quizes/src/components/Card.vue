980c3f623e2b0bb651b0c541a80ff7fdc2df68c9
Piotr Archi <piotr.borysowski@gmail.com>
2024-05-22 16:33:20 +0200

    quiz game

---
+++ quizes/src/components/Card.vue
@@ -0,0 +1,39 @@
+<script setup>
+const { quiz } = defineProps(["quiz"]);
+</script>
+
+<template>
+    <div class="card">
+        <img :src="quiz.img" alt="" />
+        <div class="card-text">
+            <h2>{{ quiz.name }}</h2>
+            <p>{{ quiz.questions.length }} questions</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.card {
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 1px #aaa;
+    margin-bottom: 35px;
+    margin-top: 20px;
+    cursor: pointer;
+}
+
+.card img {
+    width: 100%;
+    height: 190px;
+    margin: 0;
+}
+
+.card .card-text {
+    padding: 10px 5px;
+}
+
+.card .card-text h2 {
+    font-weight: bold;
+}
+</style>
